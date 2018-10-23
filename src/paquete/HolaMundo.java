96d21d2c80ed362a40f34713fd1443b09f1d4d7a
ramonlau <44288234+ramonlau@users.noreply.github.com>
2018-10-23 18:22:04 +0200

    Primera version Hola Mundo

---
+++ src/paquete/HolaMundo.java
@@ -0,0 +1,9 @@
+package paquete;
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/paquete/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author raguilar
+ *
+ */
+package paquete;
