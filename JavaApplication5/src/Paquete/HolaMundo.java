3e7d188a2f90f015b634f58b6beba2cc0253b33f
ramonlau <44288234+ramonlau@users.noreply.github.com>
2018-10-26 12:41:30 +0200

    desde cole

---
+++ JavaApplication5/src/Paquete/HolaMundo.java
@@ -0,0 +1,13 @@
+package Paquete;
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hola Mundo");
+        System.out.println("Me llamo Hal");
+        System.out.println("Repositorio local cole");
+        System.out.println("Mismo ejemplo");
+        System.out.println("otro ejemplo");
+	}
+
+}
